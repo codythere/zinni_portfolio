d8debe03a756ddb48fc26000e5d9e79172b75b54
Zinni Lai <fk2946@yahoo.com.tw>
2023-08-20 20:15:57 +0800

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -161,33 +161,6 @@ const experiences = [
   },
 ];
 
-const testimonials = [
-  {
-    testimonial:
-      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-    name: "Sara Lee",
-    designation: "CFO",
-    company: "Acme Co",
-    image: "https://randomuser.me/api/portraits/women/4.jpg",
-  },
-  {
-    testimonial:
-      "I've never met a web developer who truly cares about their clients' success like Rick does.",
-    name: "Chris Brown",
-    designation: "COO",
-    company: "DEF Corp",
-    image: "https://randomuser.me/api/portraits/men/5.jpg",
-  },
-  {
-    testimonial:
-      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: "Lisa Wang",
-    designation: "CTO",
-    company: "456 Enterprises",
-    image: "https://randomuser.me/api/portraits/women/6.jpg",
-  },
-];
-
 const projects = [
   {
     name: "Raven Express (前台)",
@@ -208,8 +181,8 @@ const projects = [
       },
     ],
     image: ravenexpressfront,
-    showcase_link: "https://raven-express-front.vercel.app/",
-    source_code_link: "https://github.com/ziynnyiy/raven-express-front",
+    showcase_link: "https://raven-express.vercel.app/",
+    source_code_link: "https://github.com/ziynnyiy/raven-express",
   },
   {
     name: "Raven Express (後台)",
